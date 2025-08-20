9156c81faadd44f3f791528d5f279d4fd95276b8
bonsoul <bonsoul24@gmail.com>
2025-08-20 12:15:50 +0300

    CASE STUDIES

--- CASE STUDY2.R
+++ CASE STUDY2.R
@@ -20,12 +20,20 @@ str(df)
 # 2. Check for zero variance variables
 sapply(df, function(x) var(as.numeric(x), na.rm = TRUE))
 
+
+
+df1_std <- df1 %>%
+  mutate(across(c(sleep_bal, relaxed, self_time,
+                  prod_inc, time_bp, time_dp, travel_time),
+                scale))
+
+
 # 3. Fit a simpler model first (just to test)
 library(lavaan)
 test_model <- '
   wellbeing =~ sleep_bal + relaxed + self_time
 '
-test_fit <- sem(test_model, data = df1, std.lv = TRUE)
+test_fit <- sem(test_model, data = df1_std, std.lv = TRUE)
 summary(test_fit, fit.measures = TRUE, standardized = TRUE)
 
 
@@ -33,21 +41,17 @@ summary(test_fit, fit.measures = TRUE, standardized = TRUE)
 
 # Structural Model (SEM)
 
-sem_model <- '
-  # Measurement part
+# Simplified SEM with just measurement part (like your first model)
+sem_model_reduced <- '
   Wellbeing =~ sleep_bal + relaxed + self_time
-  Productivity =~ prod_inc + time_bp + time_dp + travel_time
-
-  # Structural part (hypothesis)
-  Productivity ~ Wellbeing
+  Productivity =~ prod_inc + time_bp + travel_time
 '
+sem_fit_reduced <- sem(sem_model_reduced, data = df1, std.lv = TRUE)
+summary(sem_fit_reduced, fit.measures = TRUE, standardized = TRUE)
 
-sem_fit <- sem(sem_model, data = df1, std.lv = TRUE)
-
-summary(sem_fit, fit.measures = TRUE, standardized = TRUE)
 
 
 # Modification indices (suggests extra paths)
-modificationindices(sem_fit, sort.=TRUE, maximum.number=10)
+modificationindices(sem_fit_reduced, sort.=TRUE, maximum.number=10)
 
 
